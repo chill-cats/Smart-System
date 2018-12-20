2c7e0cd5c147030ea148f724bc7a564e18841334
Nhan <lesusu850@gmail.com>
2018-12-20 18:40:01 +0700

    Add multiWiFi

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,28 @@
  */
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiMulti.h>
+#include <ESPmDNS.h>
 #include <freertos/FreeRTOS.h>
 
-const char* SSID = "HELLO";
-const char* PASS = "HI";
-
+WiFiMulti wifiMulti;
 
 void setup() {
 	Serial.begin(115200);
+	WiFi.mode(WIFI_STA);
 
+	wifiMulti.addAP("ssid_from_AP_1", "your_password_for_AP_1");
+	wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
+	wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+
+	Serial.println("Connecting Wifi...");
+	if (wifiMulti.run() == WL_CONNECTED) {
+		Serial.println("");
+		Serial.println("WiFi connected");
+		Serial.println("IP address: ");
+		Serial.println(WiFi.localIP());
+	}
 }
 void loop() {
-
 }
-
-
 
