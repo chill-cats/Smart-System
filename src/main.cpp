0b8f26f54ed8eb4567b4d6c4bf7d0b9a69710062
Nhan <lesusu850@gmail.com>
2018-12-20 23:57:14 +0700

    Prepare WiFi connections

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,66 @@
-/*
- * main.cpp
- *
- *  Created on: Dec 20, 2018
- *      Author: nhan
- */
 #include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <ESPmDNS.h>
-#include <freertos/FreeRTOS.h>
+
+void taskOne(void* taskOne);
+void taskTwo(void* taskTwo);
 
 WiFiMulti wifiMulti;
 
 void setup() {
-	Serial.begin(115200);
-	WiFi.mode(WIFI_STA);
+	Serial.begin(112500);
+	pinMode(2, OUTPUT);
 
-	wifiMulti.addAP("ssid_from_AP_1", "your_password_for_AP_1");
-	wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
-	wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+	wifiMulti.addAP("DD_WRT", "NguyenThiHongTho");
+	wifiMulti.addAP("second_ap", "second_ap_password");
+	wifiMulti.addAP("third_ap", "third_ap_password");
+	delay(1000);
 
-	Serial.println("Connecting Wifi...");
-	if (wifiMulti.run() == WL_CONNECTED) {
-		Serial.println("");
-		Serial.println("WiFi connected");
-		Serial.println("IP address: ");
-		Serial.println(WiFi.localIP());
+	while(wifiMulti.run() != WL_CONNECTED) {
+		Serial.println(".");
+		delay(100);
 	}
+	Serial.print("Connected to");
+	Serial.println(WiFi.SSID());
+
+	delay(1000);
+
+
+
+	xTaskCreate(taskOne, /* Task function. */
+				"TaskOne", /* String with name of task. */
+				10000, /* Stack size in bytes. */
+				NULL, /* Parameter passed as input of the task */
+				1, /* Priority of the task. */
+				NULL); /* Task handle. */
+
+	xTaskCreate(taskTwo, /* Task function. */
+				"TaskTwo", /* String with name of task. */
+				10000, /* Stack size in bytes. */
+				NULL, /* Parameter passed as input of the task */
+				1, /* Priority of the task. */
+				NULL); /* Task handle. */
+
 }
+
 void loop() {
 }
 
+void taskOne(void * parameter) {
+	while (true) {
+		digitalWrite(2, HIGH);
+		vTaskDelay(1000);
+		digitalWrite(2, LOW);
+		vTaskDelay(1000);
+	}
+}
+
+void taskTwo(void * parameter) {
+	while (true) {
+		Serial.println("Hello from task 2");
+		vTaskDelay(1000);
+	}
+}
