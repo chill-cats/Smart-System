c363c8112fba9b5a11e261f6f40cc588f32f9149
Nhan <lesusu850@gmail.com>
2018-12-22 11:20:11 +0700

    Add send HTTP GET code

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,27 @@
 #include <ESPmDNS.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <HTTPClient.h>
+
+#define RFID_SCAN 0
+#define LIGHT_ON 1
+#define LIGHT_OFF 2
 
 void rfidCheck(void* pvParameters);
 void taskTwo(void* taskTwo);
+void sendHTTPRequest(int mode);
+String httpRequestBuilder(int mode);
+String UID = "";
 
-const char *className = "11T";
-
+String className = "11T";
 const int resetPin = 22; // Reset pin
 const int ssPin = 21; // Slave select pin
 
+byte rfUID[4];
+
 WiFiMulti wifiMulti;
 MFRC522 mfrc522(ssPin, resetPin); // Create instance
-QueueHandle_t queue;
+
 void setup() {
 	Serial.begin(112500);
 	SPI.begin();
@@ -39,8 +48,6 @@ void setup() {
 
 	delay(1000);
 	mfrc522.PCD_DumpVersionToSerial();
-
-	queue = xQueueCreate(10, sizeof(long));
 
 	xTaskCreate(rfidCheck, /* Task function. */
 	"rfidCheckingTask", /* String with name of task. */
@@ -68,9 +75,13 @@ void rfidCheck(void *pvParameters) {
 		if (mfrc522.PICC_IsNewCardPresent()) {
 			if (mfrc522.PICC_ReadCardSerial()) {
 				mfrc522.PICC_DumpToSerial(&mfrc522.uid);
+				for (int i = 0; i <= 4; i++) {
+					rfUID[i] = mfrc522.uid.uidByte[i];
+					UID = UID + String(rfUID[i]);
+				}
+				sendHTTPRequest(RFID_SCAN);
 			}
 		}
-
 	}
 }
 
@@ -78,5 +89,29 @@ void taskTwo(void * parameter) {
 	while (true) {
 		Serial.println("Hello from task 2");
 		vTaskDelay(1000);
+	}
+}
+String httpRequestBuilder(int mode) {
+	String httpRequest = "";
+	switch (mode) {
+	case RFID_SCAN:
+		httpRequest = String(String("192.168.1.103:2705") + String("/") + String(className) + String("/") + String(UID));
+		UID = "";
+		break;
+	case LIGHT_ON:
+		httpRequest = String(String("192.168.1.103:2705") + String("/") + String(":") + String(className) + String("/ON"));
+		break;
+	case LIGHT_OFF:
+		httpRequest = String(String("192.168.1.103:2705") + String("/") + String(":") + String(className) + String("/OFF"));
+	}
+	Serial.println(httpRequest);
+	return httpRequest;
+}
+void sendHTTPRequest(int mode) {
+	HTTPClient http;
+	http.begin(httpRequestBuilder(mode));
+	int http_code = http.GET();
+	if (http_code > 0) {
+		Serial.println("Error sending http GET");
 	}
 }
