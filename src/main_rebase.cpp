64f6b6ca0f3842beb4e9c09f8325820ebaefbb63
Le Nguyen Hoang Nhan <lesusu850@gmail.com>
2019-01-01 22:22:06 +0700

    Rewrite Code, More Robust and Faster code.

--- src/main_rebase.cpp
+++ src/main_rebase.cpp
@@ -15,31 +15,32 @@
 #include <MFRC522.h>
 #include <HTTPClient.h>
 // Define value
-
+#define RFID_SCAN 0
+#define PEOPLE_PRESENT 1
+#define PEOPLE_NOT_PRESENT 2
 // Function declaration
 void RFIDCheckTask(void *pvParameters);
-void sendHTTPRequest(String UID);
+void sendHTTPRequest(String UID, int mode);
 void blockRFIDReader(int blockTime);
 void blockRFIDTask(void *pvParameters);
 void setup();
 void loop();
+void HTTPResponseHandler(String HTTPResponse);
 
-String readHTTPReponse(int delayTime);
-String HTTPResponseHandler(String HTTPResponse);
-String HTTPRequestBuilder();
+String readHTTPResponse(int delayTime);
+String HTTPRequestBuilder(String UID, int mode);
 
 // Constant Variable Declaration
-const String serverIP = "112.197.235.17";
+const char *serverIP = "112.197.235.17";
 const String className = "11T";
 const String UUID = "0e47f9cf-ac45-46a8-baeb-c5ec048ceec3";
 const int serverPORT = 2705;
-const int MFRC522RstPin = 22;
-const int MFRC522SsPin = 21;
+const int MFRC522RstPin = 13;
+const int MFRC522SsPin = 12;
 
 // Normal Variable Declaration
 byte rfUID[4];
 int FLAG_ISBLOCKED = 0;
-String card_UID = "";
 
 WiFiMulti wifiMulti;
 MFRC522 mfrc522(MFRC522SsPin, MFRC522RstPin);
@@ -65,17 +66,115 @@ void setup() {
 	Serial.println(WiFi.SSID());
 
 	// freeRTOS time!!
-	xTaskCreate(RFIDCheckTask, /* Task main function */
-	"RFIDCheckTask", /* Name of Task */
-	30000, /* Task size in bytes */
-	NULL, /* Parameter passed as input of the task */
-	3, /* Priority of the task */
-	NULL); /* Task handle */
+	xTaskCreate(RFIDCheckTask, /* Task main function */"RFIDCheckTask", /* Name of Task */
+	30000, /* Task size in bytes */NULL, /* Parameter passed as input of the task */
+	3, /* Priority of the task */NULL); /* Task handle */
 }
 void loop() {
+	vTaskDelay(10);
 } // end loop
 void RFIDCheckTask(void *pvParameters) {
+	String card_UID = "";
 	while (1) {
-
+		vTaskDelay(10);
+		if (FLAG_ISBLOCKED == 0 && mfrc522.PICC_IsNewCardPresent()
+				&& mfrc522.PICC_ReadCardSerial()) {
+			for (int i = 0; i < 4; i++) {
+				rfUID[i] = mfrc522.uid.uidByte[i];
+				card_UID = card_UID + String(rfUID[i]);
+			}
+			Serial.println(card_UID);
+			sendHTTPRequest(card_UID, RFID_SCAN);
+			card_UID = "";
+			mfrc522.PICC_HaltA();
+			mfrc522.PCD_StopCrypto1();
+		}
+	} // end infinite loop;
+} // end RFIDCheckTask
+void sendHTTPRequest(String UID, int mode) {
+	if (!wifiClient.connect(serverIP, serverPORT)) {
+		Serial.println("Connect to host failed");
+		return;
+	} else {
+		wifiClient.print(HTTPRequestBuilder(UID, mode));
 	}
-} //end RFIDCheckTask
+	Serial.println("");
+	HTTPResponseHandler(readHTTPResponse(5000));
+
+} // end sendHTTPRequest
+
+String HTTPRequestBuilder(String UID, int mode) {
+	String httpRequest = "";
+	switch (mode) {
+	case RFID_SCAN:
+		httpRequest = String("GET ") + "/:" + className + "/" + UID
+				+ " HTTP/1.1\r\n" + "Host: " + serverIP + "\r\n"
+				+ "Connection: close \r\n\r\n";
+		break;
+	case PEOPLE_PRESENT:
+		httpRequest = String("GET ") + "/:" + className + "/" + "ON"
+				+ " HTTP/1.1\r\n" + "Host: " + serverIP + "\r\n"
+				+ "Connection: close\r\n\r\n";
+		break;
+	case PEOPLE_NOT_PRESENT:
+		httpRequest = String("GET ") + "/:" + className + "/" + "OFF"
+				+ " HTTP/1.1\r\n" + "Host: " + serverIP + "\r\n"
+				+ "Connection: close\r\n\r\n";
+		break;
+	}
+	Serial.println("HTTP REQUEST IS:");
+	Serial.println(httpRequest);
+	return httpRequest;
+}
+
+String readHTTPResponse(int delayTime) {
+	String httpResponse = "";
+	while (wifiClient.available() == 0) {
+		unsigned long timeOut = millis();
+		if (millis() - timeOut > 5000) {
+			Serial.println("NO HTTP RESPONSE RECIEVED ON TIME");
+		}
+	}
+	Serial.println("Recieved:");
+	while (wifiClient.available()) {
+		httpResponse = wifiClient.readString();
+		Serial.println(httpResponse);
+	}
+	return httpResponse;
+}
+
+void HTTPResponseHandler(String httpResponse) {
+	String header;
+	String body;
+	String content;
+	header = httpResponse.substring(0, httpResponse.indexOf("\n"));
+	Serial.println(header);
+	if (header.indexOf("200 OK")) {
+		body = httpResponse.substring(httpResponse.indexOf("\n"));
+		content = body.substring(body.indexOf("GMT") + 7);
+		if (content.indexOf("OK") != -1) {
+			Serial.println("Content: OK");
+
+		} else if (content.indexOf("NOTFOUND") != -1) {
+			Serial.println("Content: NOTFOUND");
+			Serial.println("Block RFID for 10s");
+			blockRFIDReader(10000);
+
+		} else if (content.indexOf("ERROR") != -1) {
+			Serial.println("Content: ERROR");
+			Serial.println("Block RFID for 30s");
+			blockRFIDReader(30000);
+		}
+	}
+}
+void blockRFIDReader(int blockTime) {
+	xTaskCreate(blockRFIDTask, "blockRFIDTask", 1000, (void *) blockTime, 1,
+	NULL);
+}
+void blockRFIDTask(void *pvParameters) {
+	int delayTime = (int) pvParameters;
+	FLAG_ISBLOCKED = 1;
+	vTaskDelay(delayTime);
+	FLAG_ISBLOCKED = 0;
+	vTaskDelete(NULL);
+}
